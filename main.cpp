51ce8ee8c89a30ca31f0e1fd5b52d30981be36ac
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-12-31 19:43:44 +0300

    added

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void drawLine(int countSpaces, int countStars) {
+	for(int i = 0; i < countSpaces; i++) {
+		cout << " ";
+	}
+	for(int j = 0; j < countStars; j++) {
+		cout << "*";
+	}
+	cout << endl;
+}
+
+void drawTree(int countLines) {
+	for(int j = 1; j < countLines; j++) {
+		drawLine(countLines - j, j*2 - 1);
+	}
+}
+
+int main(){
+	cout << "ВЯЧЕСЛАВ ВАДИМОВИЧ, ОТ ИМЕНИ ГРУППЫ ИУ8-33 ПОЗДРАВЛЯЮ ВАС С НАСТУПАЮЩИМ 2018 ГОДОМ!!!" << endl;
+	cout << "ЖЕЛАЕМ ВАМ СЧАСТЬЯ, ЗДОРОВЬЯ И ЧТОБЫ ВАШИ СТУДЕНТЫ РАДОВАЛИ ВАС СВОИМИ УСПЕХАМИ!!!" << endl;
+	cout << "И ЧТОБЫ ВЫ МОГЛИ ПОЧУСТВОВАТЬ АТМОСФЕРУ ПРАЗДНИКА, МЫ НАРИСОВАЛИ ДЛЯ ВАС ЕЛОЧКУ!!!" << endl;
+	for(int i = 0; i < 3; i++){
+		drawTree(20);	
+	}
+	return 0;
+}
